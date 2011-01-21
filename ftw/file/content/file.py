501c41caf32f2c457666f891ef867e232714f880
zope <zope@tau.4teamwork.ch>
2011-01-21 11:08:26 +0100

    fix permissions on date field

--- ftw/file/content/file.py
+++ ftw/file/content/file.py
@@ -8,8 +8,7 @@ from logging import getLogger
 from plone.app.blob.field import BlobMarshaller
 from Products.Archetypes import atapi
 from Products.ATContentTypes.content.file import ATFile, ATFileSchema
-from Products.CMFCore.permissions import ManagePortal
-from Products.CMFCore.permissions import View
+from Products.CMFCore.permissions import ManagePortal, ModifyPortalContent, View
 from Products.CMFCore.utils import getToolByName
 from Products.validation import V_REQUIRED
 from ZODB.POSException import ConflictError
@@ -62,7 +61,9 @@ if 'effectiveDate' in FileSchema.keys():
         
     FileSchema['effectiveDate'].default_method = DateTime
     FileSchema['effectiveDate'].widget.show_hm = False
+    FileSchema['effectiveDate'].write_permission = ModifyPortalContent
 
+    FileSchema['relatedItems'].write_permission = ModifyPortalContent
 
 class File(ATFile):
     """A file content type based on blobs.
