d33642a4473abd9658f849d19615483aa73b1020
Thomas Buchberger <t.buchberger@4teamwork.ch>
2011-06-22 17:09:07 +0200

    Added versions migrator.

--- ftw/file/migration.py
+++ ftw/file/migration.py
@@ -15,6 +15,14 @@ from Products.CMFCore.utils import getToolByName
 from Products.contentmigration.catalogpatch import applyCatalogPatch
 from Products.contentmigration.catalogpatch import removeCatalogPatch
 from Products.contentmigration.common import HAS_LINGUA_PLONE
+from Products.contentmigration.archetypes import BaseATMigrator
+from zope.app.component.hooks import getSite
+from zope.component import getUtility
+from Acquisition import aq_inner
+from zope.component.interfaces import IFactory
+from Products.Archetypes.interfaces import IReferenceable
+from Products.Archetypes.ArchetypeTool import getType
+from Products.Archetypes.config import REFERENCE_ANNOTATION
 
 
 # # migration of file content to blob content type
@@ -35,6 +43,133 @@ class FtwFileMigrator(BaseMigrator):
     def last_migrate_reindex(self):
         self.new.reindexObject(idxs=['object_provides', 'portal_type',
             'Type', 'UID'])
+
+    def last_migrate_versions(self):
+        atool = getToolByName(self.old, 'portal_archivist')
+        history = atool.getHistory(self.old)
+        for version in history:
+            obj = version.data.object
+            migrator = FtwFileVersionMigrator(obj)
+            migrator.migrate()
+            
+
+class FtwFileVersionMigrator(BaseATMigrator):
+    src_portal_type = 'ftw_File'
+    src_meta_type = 'ftw_File'
+    dst_portal_type = 'File'
+    dst_meta_type = 'FtwFile'
+
+    # migrate all fields except 'file', which needs special handling...
+    fields_map = {
+        'file': None,
+    }
+
+    def renameOld(self):
+        """Renames the old object
+        """
+        setattr(self.parent, self.old_id, self.old)
+        delattr(self.parent, self.orig_id)
+
+    def createNew(self):
+        """Create the new object
+        """
+        portal = getSite()
+        typesTool = getToolByName(portal, 'portal_types')
+        fti = typesTool.getTypeInfo(self.dst_portal_type)
+        factory = getUtility(IFactory, fti.factory)
+        obj = factory(self.new_id, **self.schema)
+        if hasattr(obj, '_setPortalTypeName'):
+            obj._setPortalTypeName(self.getId())
+        setattr(self.parent, self.new_id, obj)
+        self.new = getattr(aq_inner(self.parent).aq_explicit, self.new_id)
+
+    def remove(self):
+        """Removes the old item
+        """
+        delattr(self.parent, self.old_id)
+
+    def reorder(self):
+        """Reorder the new object in its parent"""
+        return
+
+    def beforeChange_schema(self):
+        """Load the values of fields from according to fields_map if present.
+        Each key in fields_map is a field in the old schema and each
+        value is a field in the new schema.  If fields_map isn't a
+        mapping, each filed in the old schema will be migrated into
+        the new schema.  Obeys field modes for readable and writable
+        fields.  These values are then passed in as field kwargs into
+        the constructor in the createNew method."""
+
+        old_schema = self.old.Schema()
+
+        portal = getSite()
+        typesTool = getToolByName(portal, 'portal_types')
+        fti = typesTool.getTypeInfo(self.dst_portal_type)
+        archetype = getType(self.dst_meta_type, fti.product)
+        new_schema = archetype['klass'].schema 
+
+        if self.only_fields_map:
+            old_field_names = self.fields_map.keys()
+        else:
+            old_field_names = old_schema.keys()
+
+        # Let the migrator handle the id and dates
+        for omit_field_name in ['id', 'creation_date',
+                                'modification_date']:
+            if omit_field_name in old_field_names:
+                old_field_names.remove(omit_field_name)
+
+        kwargs = getattr(self, 'schema', {})
+        for old_field_name in old_field_names:
+            old_field = self.old.getField(old_field_name)
+            new_field_name = self.fields_map.get(old_field_name,
+                                                 old_field_name)
+
+            if new_field_name is None:
+                continue
+
+            new_field = new_schema.get(new_field_name, None)
+            if new_field is None:
+                continue
+
+            if ('r' in old_field.mode and 'w' in new_field.mode):
+                accessor = (
+                    getattr(old_field, self.accessor_getter)(self.old)
+                    or old_field.getAccessor(self.old))
+                value = accessor()
+                kwargs[new_field_name] = value
+        self.schema = kwargs
+
+    def migrate_owner(self):
+        """Migrates the zope owner
+        """
+        self.new._owner = self.old.getOwner(info=1)
+
+    def migrate_localroles(self):
+        """Migrate local roles
+        """
+        return
+
+    def migrate_user_roles(self):
+        """Migrate roles added by users
+        """
+        return
+
+    def migrate_at_uuid(self):
+        """Migrate AT universal uid
+        """
+        if not IReferenceable.providedBy(self.old):
+            return # old object doesn't support AT uuids
+        uid = self.old.UID()
+        self.new._setUID(uid)
+
+    def migrate_references(self):
+        """Migrate references annotation."""
+        # Restor the references annotation
+        if hasattr(self, REFERENCE_ANNOTATION):
+            at_references = getattr(self, REFERENCE_ANNOTATION)
+            setattr(self.new, REFERENCE_ANNOTATION, at_references)
 
 
 class FtwFileMigrationWalker(CustomQueryWalker):
