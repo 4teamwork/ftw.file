3605d2cd21795b4ca7cceedf7a9728c88a94cb02
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2011-11-28 17:46:13 +0100

    prepare package for github

---
+++ README.rst
@@ -0,0 +1,37 @@
+Overview
+========
+
+This is a file content for plone which provides some usefull functions, 
+such as:
+- Write downloader-name in history (ftw.journal)
+- Image preview
+- New FileField (stores more informations in the RESPONSE header)
+
+
+Install
+======
+
+- Add ``ftw.file`` to yout buidlout configuration
+
+:: 
+  [instance]
+  eggs = 
+    ftw.file
+    
+- Run buildout
+
+- Install ``ftw.file`` in portal_setup
+
+
+Links
+=====
+
+- Package repository: https://github.com/4teamwork/ftw.file
+- Issue tracker: https://github.com/4teamwork/ftw.file/issues
+- Package on pypi: http://pypi.python.org/pypi/ftw.file
+
+
+Maintainer
+==========
+
+This package is produced and maintained by `4teamwork GmbH <http://www.4teamwork.ch/>`_.
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = open('ftw/file/version.txt').read().strip()
+version = '1.4.5dev'
 maintainer = 'Thomas Buchberger'
 
 tests_require = [
@@ -10,20 +10,22 @@ tests_require = [
 
 setup(name='ftw.file',
       version=version,
-      description="A file content type",
-      long_description=open("README.txt").read() + "\n" +
+      description="A file content type for gov usecases",
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+        "Framework :: Plone :: 4.1",
         "Framework :: Plone",
         "Programming Language :: Python",
         ],
-      keywords='',
-      author='Thomas Buchberger',
-      author_email='t.buchberger@4teamwork.ch',
-      url='http://www.4teamwork.ch',
-      license='GPL',
+      keywords='plone file contenttype',
+      maintainer=maintainer,
+      author='4teamwork GmbH',
+      author_email='info@4teamwork.ch',
+      url='https://github.com/4teamwork/ftw.file',
+      license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw'],
       include_package_data=True,
