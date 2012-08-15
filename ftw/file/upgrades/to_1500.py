27af8b3220f0967c8b0b320c145fa0978a60c0e7
Timon Tschanz <timon.tschanz@4teamwork.ch>
2012-08-15 16:26:16 +0200

    added upgrade_step

---
+++ ftw/file/upgrades/to_1500.py
@@ -0,0 +1,28 @@
+from ftw.upgrade import ProgressLogger
+from ftw.upgrade import UpgradeStep
+from zope.app.component.hooks import getSite
+import logging
+from Products.CMFCore.utils import getToolByName
+from DateTime import DateTime
+LOG = logging.getLogger('ftw.workspace.upgrades')
+
+
+class AddDocumentDateIndex(UpgradeStep):
+
+
+    def __call__(self):
+        self.add_document_index()
+
+    def add_document_index(self):
+        site = getSite()
+        catalog = getToolByName(site, 'portal_catalog')
+        self.catalog_add_index('document_date', 'DateIndex')
+        results = catalog.search({'portal_type':'File'})
+        with ProgressLogger('Reindex ftw.files', results) as step:
+            for brain in results:
+                obj = brain.getObject()
+                if not obj.getDocument_date():
+                    obj.setDocument_date(obj.getEffectiveDate())
+                    obj.setEffectiveDate(DateTime())                
+                obj.reindexObject(idxs=['effective', 'document_date'])
+                step()
