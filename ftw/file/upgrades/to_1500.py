b46ad62521b05647f62b08b3b72a55bac3ddafea
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2012-10-01 10:40:13 +0200

    pep8/pylint - to_1500

--- ftw/file/upgrades/to_1500.py
+++ ftw/file/upgrades/to_1500.py
@@ -4,25 +4,24 @@ from zope.app.component.hooks import getSite
 import logging
 from Products.CMFCore.utils import getToolByName
 from DateTime import DateTime
-LOG = logging.getLogger('ftw.workspace.upgrades')
+LOG = logging.getLogger('ftw.file.upgrades')
 
 
 class AddDocumentDateIndex(UpgradeStep):
 
-
     def __call__(self):
-        self.add_document_index()
+        self.add_document_index_and_migrate()
 
-    def add_document_index(self):
+    def add_document_index_and_migrate(self):
         site = getSite()
         catalog = getToolByName(site, 'portal_catalog')
         self.catalog_add_index('document_date', 'DateIndex')
-        results = catalog.search({'portal_type':'File'})
+        results = catalog.search({'portal_type': 'File'})
         with ProgressLogger('Reindex ftw.files', results) as step:
             for brain in results:
                 obj = brain.getObject()
                 if not obj.getDocument_date():
                     obj.setDocument_date(obj.getEffectiveDate())
-                    obj.setEffectiveDate(DateTime())                
+                    obj.setEffectiveDate(DateTime())
                 obj.reindexObject(idxs=['effective', 'document_date'])
                 step()
