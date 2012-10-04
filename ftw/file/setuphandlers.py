e634b8578bbafcc097452c6242363776e7a6b3bd
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2012-10-04 14:33:04 +0200

    add index documentDate

--- ftw/file/setuphandlers.py
+++ ftw/file/setuphandlers.py
@@ -1,19 +1,28 @@
 from Products.CMFCore.utils import getToolByName
-from Products.ZCatalog.Catalog import CatalogError
 from ftw.file.config import INDEXES
 
 
-def add_indexes(site):
+def add_indexes(context, logger):
     """Add our indexes to the catalog.
 
     Doing it here instead of in profiles/default/catalog.xml means we
     do not need to reindex those indexes after every reinstall.
     """
 
-    context = site.getSite()
     catalog = getToolByName(context, 'portal_catalog')
     indexes = catalog.indexes()
 
     for name, meta_type in INDEXES:
         if name not in indexes:
             catalog.addIndex(name, meta_type)
+            logger.info('Added index: %s' % name)
+
+def import_various(context):
+    """Import step for configuration that is not handled in xml files.
+    """
+    # Only run step if a flag file is present
+    if context.readDataFile('ftw.file.setuphandlers.txt') is None:
+        return
+    logger = context.getLogger('ftw.file')
+    site = context.getSite()
+    add_indexes(site, logger)
