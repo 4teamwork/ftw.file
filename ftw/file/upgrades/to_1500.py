e0c953ebf8126907e57b1f1fbfda7f2c3bd23a53
zope <zope@0000islx04.(none)>
2012-10-08 19:18:43 +0200

    fix migration step for documentdate

--- ftw/file/upgrades/to_1500.py
+++ ftw/file/upgrades/to_1500.py
@@ -1,32 +1,38 @@
+from DateTime import DateTime
+from Products.CMFCore.utils import getToolByName
 from ftw.upgrade import ProgressLogger
 from ftw.upgrade import UpgradeStep
+from zope.annotation import IAnnotations
 from zope.app.component.hooks import getSite
-import logging
-from Products.CMFCore.utils import getToolByName
-from DateTime import DateTime
-LOG = logging.getLogger('ftw.file.upgrades')
+
+
+ANN_KEY = 'ftw.file-upgrade-1500-migrated-data'
 
 
 class AddDocumentDateIndex(UpgradeStep):
 
     def __call__(self):
         self.setup_install_profile('profile-ftw.file.upgrades:1500')
-        self.add_document_index_and_migrate()
-
-    def add_document_index_and_migrate(self):
-        site = getSite()
-        catalog = getToolByName(site, 'portal_catalog')
         if not self.catalog_has_index('documentDate'):
             self.catalog_add_index('documentDate', 'DateIndex')
-        results = catalog.search({'portal_type': 'File'})
-        with ProgressLogger('Reindex ftw.files', results) as step:
-            for brain in results:
-                obj = brain.getObject()
-                if not obj.getDocumentDate():
-                    newdate = obj.getEffectiveDate()
-                    if not newdate:
-                        newdate = obj.created()
-                    obj.setDocumentDate(newdate)
-                    obj.setEffectiveDate(DateTime())
-                obj.reindexObject(idxs=['effective', 'documentDate'])
+        self.migrate_dates()
+        self.catalog_reindex_objects({'portal_type': 'File'}, idxs=['effective', 'documentDate'])
+
+    def migrate_dates(self):
+        objects = self.catalog_unrestricted_search({'portal_type': 'File'}, full_objects=True)
+
+        with ProgressLogger('Migrate file dates', objects) as step:
+            for obj in objects:
+                ann = IAnnotations(obj)
+                if ann.get(ANN_KEY, None) is not None:
+                    step()
+                    continue
+
+                ann[ANN_KEY] = True
+
+                newdate = obj.getEffectiveDate()
+                if not newdate:
+                    newdate = obj.created()
+                obj.setDocumentDate(newdate)
+                obj.setEffectiveDate(DateTime())
                 step()
