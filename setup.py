1362b6f39b33324d4ab99efddb94abecf91429e4
Jonas Baumann <jone@jone.ch>
2012-11-12 14:17:05 +0100

    Update package metadata.

--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ maintainer = 'Thomas Buchberger'
 tests_require = [
     'plone.app.testing',
     'plone.mocktestcase',
-]
+    ]
 
 setup(name='ftw.file',
       version=version,
       description="A file content type for gov usecases",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.rst").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -21,26 +22,30 @@ setup(name='ftw.file',
         "Framework :: Plone",
         "Programming Language :: Python",
         ],
+
       keywords='plone file contenttype',
       maintainer=maintainer,
       author='4teamwork GmbH',
-      author_email='info@4teamwork.ch',
+      author_email='mailto:info@4teamwork.ch',
       url='https://github.com/4teamwork/ftw.file',
       license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw'],
       include_package_data=True,
       zip_safe=False,
+
       install_requires=[
-          'setuptools',
-          'ftw.journal',
-          'plone.app.registry',
-          'ftw.upgrade',
-          'ftw.calendarwidget'
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        'ftw.journal',
+        'plone.app.registry',
+        'ftw.upgrade',
+        'ftw.calendarwidget'
+        # -*- Extra requirements: -*-
+        ],
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
+
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
