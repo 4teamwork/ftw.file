009f931ac5c368eb3b023b4307b15bebcfa0b96c
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2012-12-03 17:11:52 +0100

    add missing upgrade step - install ftw.calendarwidget

--- ftw/file/upgrades/to_1511.py
+++ ftw/file/upgrades/to_1511.py
@@ -1,8 +1,11 @@
 from ftw.upgrade import UpgradeStep
 
 
-class ChangePortalProperties(UpgradeStep):
+class ChangePortalPropertiesInstallMissing(UpgradeStep):
 
     def __call__(self):
         self.setup_install_profile(
             'profile-ftw.file.upgrades:1511')
+
+        self.setup_install_profile(
+            'profile-ftw.calendarwidget:default')
