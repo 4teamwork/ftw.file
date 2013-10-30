ce80536c1636658a777605ee4560060ba439cd8c
Timon Tschanz <timon.tschanz@4teamwork.ch>
2013-10-30 11:39:38 +0100

    update upgrade step

--- ftw/file/upgrades/to_1514.py
+++ ftw/file/upgrades/to_1514.py
@@ -3,4 +3,14 @@ from ftw.upgrade import UpgradeStep
 class ReindexIcons(UpgradeStep):
 
     def __call__(self):
-        self.catalog_rebuild_index('getIcon')
+        cat = self.portal.portal_catalog
+        all_values = cat.uniqueValuesFor('getContentType')
+        values = []
+        for value in all_values:
+            if value.startswith('image'):
+                values.append(value)
+        values.append('application/pdf')
+        results = cat({'portal_type':'File', 'getContentType': values})
+        for result in results:
+            obj = result.getObject()
+            cat.reindexObject(obj, idxs=['Title'], update_metadata=True)
