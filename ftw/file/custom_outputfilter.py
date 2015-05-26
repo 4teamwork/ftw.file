05d61c5ccd66ae517f1905a7721fe6a4fa9f5cdb
Lukas Knoepfel <shylux@gmail.com>
2015-05-26 12:18:34 +0200

    Override outputfilter to preserve @@image.

---
+++ ftw/file/custom_outputfilter.py
@@ -0,0 +1,14 @@
+from plone.outputfilters.filters.resolveuid_and_caption import ResolveUIDAndCaptionFilter
+
+class FtwFileFilter(ResolveUIDAndCaptionFilter):
+
+    def resolve_image(self, src):
+        (image, fullimage, source, description) = ResolveUIDAndCaptionFilter.resolve_image(self, src)
+
+        obj, subpath, appendix = self.resolve_link(src)
+        url = image.absolute_url()
+        if isinstance(url, unicode):
+            url = url.encode('utf8')
+        src = '/'.join([url, subpath, appendix])
+
+        return image, fullimage, src, description
