12fe3531077e634e02e42e10e0a6d8cb7ac7db6e
Elio Schmutz <e.schmutz@4teamwork.ch>
2015-07-21 15:48:54 +0200

    Add overlay for deleting items

--- ftw/file/resources/main.js
+++ ftw/file/resources/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-  uploader.init();
+    uploader.init();
+
+    $('div.sidebar a.delete-object-link').prepOverlay(
+    {
+        subtype: 'ajax',
+        filter: common_content_filter,
+        formselector: '#delete_confirmation',
+        cssclass: 'overlay-delete',
+        noform: function(el) {return $.plonepopups.noformerrorshow(el, 'redirect');},
+        redirect: $.plonepopups.redirectbasehref,
+        closeselector: '[name="form.button.Cancel"]',
+        width:'50%'
+    }
+    );
 });
