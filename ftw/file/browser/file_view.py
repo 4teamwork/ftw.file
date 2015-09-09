04c2d446a7d0f11efba847a57dc513a864b4ad0c
Jonas Baumann <jone@jone.ch>
2015-09-09 17:36:53 +0200

    Details view: refactor template binding
    By moving the template binding to python code, subclasses can still
    render the default template conditionally.

--- ftw/file/browser/file_view.py
+++ ftw/file/browser/file_view.py
@@ -1,9 +1,15 @@
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from ftw.file.utils import FileMetadata
 from Products.Five import BrowserView
 
 
 class FileView(BrowserView):
     """ View for ftw.file """
+
+    template = ViewPageTemplateFile('file_view.pt')
+
+    def __call__(self):
+        return self.template()
 
     def get_image_tag(self):
         return self.metadata.get_image_tag(
