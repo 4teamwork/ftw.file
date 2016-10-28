4ca12133a84e0b313165744f0f5f9d2e16e616c1
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2016-10-28 15:46:47 +0200

    Convert file builder to a DX file builder.
    Special: We always create NameBlobImage... so far no special issues.

--- ftw/file/tests/builders.py
+++ ftw/file/tests/builders.py
@@ -1,9 +1,10 @@
 from ftw.builder import builder_registry
-from ftw.builder.content import ATFileBuilder
+from ftw.builder.content import DXFileBuilder
 from path import Path
+from plone.namedfile.file import NamedBlobImage
 
 
-class CustomFileBuilder(ATFileBuilder):
+class CustomFileBuilder(DXFileBuilder):
 
     def attach_asset(self, filename):
         return self.attach_file_containing(
@@ -13,5 +14,8 @@ class CustomFileBuilder(ATFileBuilder):
         path = Path(__file__).dirname().realpath().joinpath('assets', filename)
         return path
 
+    def _attach_dx_file(self, content, name):
+        self.attach(NamedBlobImage(data=content, filename=name))
+        return self
 
 builder_registry.register('file', CustomFileBuilder, force=True)
