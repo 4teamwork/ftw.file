ed50d23a8b494360f4298a13e802939cee8b7f17
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2016-10-28 15:51:32 +0200

    Remove AT scale traversing tests.
    Example: `mycontent/file_mini` no longer works with DX types.

--- ftw/file/tests/test_fields.py
+++ ftw/file/tests/test_fields.py
@@ -1,12 +1,13 @@
-from ftw.builder import create
 from ftw.builder import Builder
+from ftw.builder import create
 from ftw.file.testing import FTW_FILE_FUNCTIONAL_TESTING
+from ftw.testbrowser import browsing
+from PIL import Image
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
-from unittest2 import TestCase
-from ftw.testbrowser import browsing
+from plone.rfc822.interfaces import IPrimaryFieldInfo
 from StringIO import StringIO
-from PIL import Image
+from unittest2 import TestCase
 
 
 class TestFieldFunctions(TestCase):
@@ -15,46 +16,21 @@ class TestFieldFunctions(TestCase):
 
     def setUp(self):
         setRoles(self.layer['portal'], TEST_USER_ID, ['Manager'])
-        self.file = create(Builder('file').attach_asset("testimage.jpg"))
-        self.file.getPrimaryField().createScales(self.file)
+        self.file = create(Builder('file').attach_asset(u"testimage.jpg"))
+        self.primary_info = IPrimaryFieldInfo(self.file)
+        # self.file.getPrimaryField().createScales(self.file)
 
     def test_available_sizes(self):
         expected = set(['mini', 'thumb', 'large',
-                       'listing', 'tile', 'preview', 'icon'])
-        sizes = self.file.getPrimaryField().getAvailableSizes(self.file).keys()
-        sizes = set(sizes)
+                        'listing', 'tile', 'preview', 'icon'])
+
+        sizes = set(
+            self.file.restrictedTraverse('@@images').getAvailableSizes())
         self.assertTrue(expected.issubset(sizes),
                         "The available sizes don't include default scales.")
-
-    def test_get_size(self):
-        size = self.file.getPrimaryField().getSize(self.file, 'mini')
-        self.assertEqual((200, 164), size, "Scale Sizes didn't match")
-
-        size = self.file.getPrimaryField().getSize(self.file, 'preview')
-        self.assertEqual((400, 328), size, "Scale Sizes didn't match")
-
-    def test_get_scale(self):
-        scale = self.file.getPrimaryField().getScale(self.file, 'mini')
-        self.assertEqual('file_mini', scale.__name__)
-        self.assertEqual(200, scale.width)
-        self.assertEqual(164, scale.height)
 
     @browsing
     def test_traversion(self, browser):
         page = browser.login().visit(self.file, view="@@images/file/mini")
         image = Image.open(StringIO(page.contents))
         self.assertEqual((200, 164), image.size)
-
-    @browsing
-    def test__other_traversion(self, browser):
-        page = browser.login().visit(self.file, view="file_mini")
-        image = Image.open(StringIO(page.contents))
-        self.assertEqual((200, 164), image.size)
-
-    @browsing
-    def test_traversion_equality(self, browser):
-        page = browser.login().visit(self.file, view="file_mini")
-        page2 = browser.login().visit(self.file, view="@@images/file/mini")
-        self.assertEqual(page.contents,
-                         page2.contents,
-                         "The Two Image traversel methods aren't equal")
