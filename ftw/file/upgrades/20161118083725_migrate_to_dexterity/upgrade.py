957241a7ee6b5628c30dceb56cd0f5d6f4fe4ac3
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2016-11-18 10:23:53 +0100

    Add AT ftw.file to DX ftw.file.File migration upgrade step.

--- ftw/file/upgrades/20161118083725_migrate_to_dexterity/upgrade.py
+++ ftw/file/upgrades/20161118083725_migrate_to_dexterity/upgrade.py
@@ -1,4 +1,5 @@
 from ftw.upgrade import UpgradeStep
+from ftw.upgrade.migration import InplaceMigrator
 
 
 class MigrateToDexterity(UpgradeStep):
@@ -7,3 +8,24 @@ class MigrateToDexterity(UpgradeStep):
 
     def __call__(self):
         self.install_upgrade_profile()
+        self.setup_install_profile('profile-plone.app.relationfield:default')
+        self.enable_versioning()
+        self.migrate()
+
+    def enable_versioning(self):
+        repository_tool = self.getToolByName('portal_repository')
+        repository_tool.setVersionableContentTypes('ftw.file.File')
+        repository_tool.addPolicyForContentType('ftw.file.File',
+                                                'version_on_revert')
+
+    def migrate(self):
+
+        migrator = InplaceMigrator(
+            new_portal_type='ftw.file.File',
+            ignore_fields=('originFilename', 'excludeFromNav'),
+            field_mapping={'documentDate': 'document_date'},
+        )
+
+        for obj in self.objects({'portal_type': 'File'},
+                                'Migrate ftw.file Files to dexterity'):
+            migrator.migrate_object(obj)
