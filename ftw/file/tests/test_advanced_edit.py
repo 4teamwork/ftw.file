d12a9fb1d4972c21604e8cab0118a7095dff4e61
Martin Bächtold <martin@baechtold.me>
2018-08-30 12:28:13 +0200

    Rewrite test using "ftw.testbrowser".
    Less code is better code.

--- ftw/file/tests/test_advanced_edit.py
+++ ftw/file/tests/test_advanced_edit.py
@@ -1,56 +1,23 @@
-import transaction
-
-from ftw.file.testing import FTW_FILE_FUNCTIONAL_TESTING
-from plone.app.testing import TEST_USER_ID
-from plone.app.testing import TEST_USER_NAME
-from plone.app.testing import TEST_USER_PASSWORD
-from plone.app.testing import setRoles
-from plone.testing.z2 import Browser
-from unittest2 import TestCase
-from pyquery import PyQuery
+from ftw.file.tests import FunctionalTestCase
+from ftw.testbrowser import browsing
 
 
-class TestAdvancedEdit(TestCase):
+class TestAdvancedEdit(FunctionalTestCase):
 
-    layer = FTW_FILE_FUNCTIONAL_TESTING
-
-    def setUp(self):
-        self.browser = Browser(self.layer['app'])
-        self.browser.handleErrors = False
-
-        self.portal = self.layer['portal']
-
-        transaction.commit()
-
-    def login(self):
-        self.browser.addHeader(
-            'Authorization',
-            'Basic %s:%s' % (TEST_USER_NAME, TEST_USER_PASSWORD,))
-
-    def test_no_advanced_edit(self):
-        setRoles(self.portal, TEST_USER_ID, ['Contributor', 'Editor'])
-        transaction.commit()
-        self.login()
-        self.browser.open(self.portal.absolute_url() + '/createObject?type_name=File')
-        html = PyQuery(self.browser.contents)
-        fieldsets = html('fieldset')
+    @browsing
+    def test_no_advanced_edit(self, browser):
+        self.grant('Contributor', 'Editor')
+        browser.login().open(self.portal.absolute_url() + '/createObject?type_name=File')
+        browser.css('fieldset')
+        fieldsets = browser.css('fieldset')
         self.assertEqual(len(fieldsets), 0, "We found Fieldsets. We didn't expect any'")
 
-    def test_advanced_edit(self):
-        form_tabs = ["Default",
-                     "Categorization",
-                     "Dates",
-                     "Creators",
-                     "Settings",
-                     "Ownership"
-                     ]
-        setRoles(self.portal, TEST_USER_ID, ['Contributor', 'Editor', 'Reviewer'])
-        transaction.commit()
-        self.login()
-        self.browser.open(self.portal.absolute_url() + '/createObject?type_name=File')
-        html = PyQuery(self.browser.contents)
-        fieldsets = html('fieldset')
-        self.assertEqual(len(fieldsets), 5, "We expected 5 Fieldsets got %s" % len(fieldsets))
-        legends = fieldsets('legend')
-        for legend in legends:
-            self.assertIn(legend.text, form_tabs)
+    @browsing
+    def test_advanced_edit(self, browser):
+        self.grant('Reviewer')
+        browser.login().open(self.portal.absolute_url() + '/createObject?type_name=File')
+        fieldsets = browser.css('fieldset')
+        self.assertEqual(
+            ['Default', 'Categorization', 'Dates', 'Creators', 'Settings'],
+            fieldsets.css('legend').text
+        )
