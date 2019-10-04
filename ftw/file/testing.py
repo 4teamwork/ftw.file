d1393da99ffc78879b190c185942a07e41cc9502
Daniel Jowett <d.jowett@4teamwork.ch>
2019-10-04 16:57:32 +0200

    Revert "Trying to fix test_pagination"
    This reverts commit 19bd22331d064df68fca2ef626f9daf307950b36.

--- ftw/file/testing.py
+++ ftw/file/testing.py
@@ -33,7 +33,6 @@ class FtwFileLayer(PloneSandboxLayer):
     def setUpPloneSite(self, portal):
         applyProfile(portal, 'plone.app.imaging:default')
         applyProfile(portal, 'plone.app.registry:default')
-        #applyProfile(portal, 'plone.app.versioningbehavior:default')
         applyProfile(portal, 'ftw.file:default')
         eventtesting.setUp()
 
