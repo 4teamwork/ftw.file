7aca3e762d4e9f63291c633b8d10cccb2c39dc3e
Daniel Jowett <d.jowett@4teamwork.ch>
2019-10-15 16:51:00 +0200

    Abbreviate geticon tests

--- ftw/file/tests/test_geticon.py
+++ ftw/file/tests/test_geticon.py
@@ -19,15 +19,8 @@ class TestFileName(TestCase):
         self.file_ = open(self.path, 'r')
         self.mtr = getToolByName(self.portal, 'mimetypes_registry')
 
-    @browsing
-    def test_custom_icon(self, browser):
-        # NB Plone includes all Python's 'mimetypes' in it's mimetypes_registry
-        self.mtr.manage_addMimeType('customimage type',
-                                    ['image/x-custom-image'],
-                                    ['NonsenseExtension'],
-                                    'customicon.png')
-        transaction.commit()
-
+    def add_content_with_browser(self, browser):
+        """ Using ftw.testbrowser exercises mimetypes regy better """
         browser.login(SITE_OWNER_NAME).open()
         factoriesmenu.add('File')  # fortunately ftw.file.File is before File
 
@@ -41,7 +34,18 @@ class TestFileName(TestCase):
         errors = browser.css('.error')
         self.assertEqual(0, len(errors), "Check no errors on form submission")
 
-        plonefile = self.portal['myfile-nonsenseextension']
+        return self.portal['myfile-nonsenseextension']
+
+    @browsing
+    def test_custom_icon(self, browser):
+        # NB Plone includes all Python's 'mimetypes' in it's mimetypes_registry
+        self.mtr.manage_addMimeType('customimage type',
+                                    ['image/x-custom-image'],
+                                    ['NonsenseExtension'],
+                                    'customicon.png')
+        transaction.commit()
+
+        plonefile = self.add_content_with_browser(browser)
         self.assertEqual('plone/customicon.png', plonefile.getIcon())
 
     @browsing
@@ -52,47 +56,23 @@ class TestFileName(TestCase):
                                     '')
         transaction.commit()
 
-        browser.login(SITE_OWNER_NAME).open()
-        factoriesmenu.add('File')  # fortunately ftw.file.File is before File
-
-        with open(self.path, 'r') as file_:
-            browser.fill({'Title': 'My file',
-                          'Filename': 'myfile',
-                          'File': (file_.read(),
-                                   'testfile.NonsenseExtension',
-                                   'image/x-custom-image')
-                          }).save()
-        errors = browser.css('.error')
-        self.assertEqual(0, len(errors), "Check no errors on form submission")
-
-        plonefile = self.portal['myfile-nonsenseextension']
+        plonefile = self.add_content_with_browser(browser)
         self.assertEqual('plone/image.png', plonefile.getIcon())
 
     @browsing
     def test_icon_for_contenttype_not_found(self, browser):
         # To test the behavior of our function when a contenttype is not found
         # in mimetyperegistry we add a mimetype so everything is indexed and
-        # created correctly.
+        # created correctly ...
         self.mtr.manage_addMimeType('customimage type',
                                     ['image/x-custom-image'],
                                     ['NonsenseExtension'],
                                     '')
         transaction.commit()
 
-        browser.login(SITE_OWNER_NAME).open()
-        factoriesmenu.add('File')  # fortunately ftw.file.File is before File
+        self.add_content_with_browser(browser)
 
-        with open(self.path, 'r') as file_:
-            browser.fill({'Title': 'My file',
-                          'Filename': 'myfile',
-                          'File': (file_.read(),
-                                   'testfile.NonsenseExtension',
-                                   'image/x-custom-image')
-                          }).save()
-        errors = browser.css('.error')
-        self.assertEqual(0, len(errors), "Check no errors on form submission")
-
-        # and then we remove it again so we won't find it in the registry.
+        # ... and then we remove it again so we won't find it in the registry.
         self.mtr.manage_delObjects(['image/x-custom-image'])
         transaction.commit()
 
