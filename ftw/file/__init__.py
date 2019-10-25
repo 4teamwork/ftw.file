0b6696ce1fbdfd0a2ffae797be0eaccee58a1a25
Daniel Jowett <d.jowett@4teamwork.ch>
2019-10-25 12:09:51 +0200

    Add uninstall profile

--- ftw/file/__init__.py
+++ ftw/file/__init__.py
@@ -3,3 +3,7 @@
 from zope.i18nmessageid import MessageFactory
 
 _ = fileMessageFactory = MessageFactory('ftw.file')
+
+
+def initialize(context):
+    pass
--- ftw/file/hooks.py
+++ ftw/file/hooks.py
@@ -10,6 +10,10 @@ def installed(site):
     add_indexes(site)
 
 
+def uninstalled(site):
+    remove_indexes(site)
+
+
 def add_indexes(context):
     """Add our indexes to the catalog.
 
@@ -24,3 +28,14 @@ def add_indexes(context):
         if name not in indexes:
             catalog.addIndex(name, meta_type)
             LOGGER.info('Added index: %s' % name)
+
+
+def remove_indexes(context):
+    """ Remove our indexes from the catalog """
+    catalog = getToolByName(context, 'portal_catalog')
+    indexes = catalog.indexes()
+
+    for name, meta_type in INDEXES:
+        if name in indexes:
+            catalog.delIndex(name)
+            LOGGER.info('Removed index: %s' % name)
