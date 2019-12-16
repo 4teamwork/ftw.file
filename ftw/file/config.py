ceb889df05c921df0ac97511ee62d4ee44555de8
Jonas Baumann <jone@jone.ch>
2019-12-16 12:25:43 +0100

    Plone 5: provide precompiled bundle.

--- ftw/file/config.py
+++ ftw/file/config.py
@@ -1,4 +1,10 @@
-"""Common configuration constants
-"""
+from Products.CMFPlone.utils import getFSVersionTuple
+
+
+if getFSVersionTuple() > (5, ):
+    IS_PLONE_5 = True
+else:
+    IS_PLONE_5 = False
+
 
 INDEXES = (("documentDate", "DateIndex"),)
--- ftw/file/resources/hideOverrideFilenameField.js
+++ ftw/file/resources/hideOverrideFilenameField.js
@@ -1,8 +1,14 @@
-$(function() {
+(function (factory) {
+  if (typeof define === 'function' && define.amd) {
+    require(['jquery'], factory);
+  } else {
+    factory(window.jQuery);
+  }
+}(function ($) {
 
-    $('#form-widgets-file input[type=radio]').change(function() {
-        display = ($('input#form-widgets-file-replace').is(':checked')) ? 'none' : '';
-        $('#formfield-form-widgets-filename_override').css('display', display);
-    });
+  $('#form-widgets-file input[type=radio]').change(function() {
+    display = ($('input#form-widgets-file-replace').is(':checked')) ? 'none' : '';
+    $('#formfield-form-widgets-filename_override').css('display', display);
+  });
 
-})
+}));
