bf1d6f672f27ab5e6caf7d96aa85f91d0ca4880c
Nachtalb <info@nachtalb.io>
2020-08-27 11:55:57 +0200

    Reimplement old ajax-upload view

---
+++ ftw/file/browser/upload.py
@@ -0,0 +1,28 @@
+from Products.CMFPlone.utils import safe_unicode
+from ftw.file import _
+from ftw.file.content.dxfile import BlobImageValueType
+from plone import api
+from zExceptions import BadRequest
+from zope.i18n import translate
+from zope.publisher.browser import BrowserView
+import json
+
+
+class FileUpload(BrowserView):
+    """View for handling drag-and-drop file replace"""
+
+    def __call__(self):
+        self.file = self.request.get('file')
+        if not self.file:
+            raise BadRequest('No content provided.')
+
+        self.filename = safe_unicode(self.file.filename)
+        self.file.seek(0)
+        self.context.file = BlobImageValueType(data=self.file.read(),
+                                               filename=self.filename)
+
+        change_note = translate(_('File replaced with Drag & Drop.'))
+        repository = api.portal.get_tool('portal_repository')
+        repository.save(obj=self.context, comment=change_note)
+
+        return json.dumps({'success': True})
---
+++ ftw/file/tests/test_ajax_upload.py
@@ -0,0 +1,54 @@
+from Products.CMFCore.utils import getToolByName
+from StringIO import StringIO
+from ftw.builder import Builder
+from ftw.builder import create
+from ftw.file.testing import FTW_FILE_INTEGRATION_TESTING
+from plone import api
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import setRoles
+from unittest import TestCase
+from zExceptions import BadRequest
+import json
+
+
+class TestAjaxUpload(TestCase):
+
+    layer = FTW_FILE_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+        self.file_ = create(
+            Builder('file')
+            .titled(u'Document')
+            .attach_file_containing('My PDF', name=u'test.pdf')
+        )
+
+        self.new_file = StringIO('Raw file data')
+        self.new_file.filename = u'new_file.txt'
+        self.new_file.content_type = u'text/plain'
+
+    def test_replace_file(self):
+        self.portal.REQUEST.set('file', self.new_file)
+
+        ajax_upload_view = self.file_.restrictedTraverse('ajax-upload')
+        response = json.loads(ajax_upload_view())
+
+        self.assertEqual(response['success'], True)
+
+    def test_no_file(self):
+        with self.assertRaises(BadRequest):
+            self.file_.restrictedTraverse('ajax-upload')()
+
+    def test_new_version(self):
+        portal = api.portal.get()
+        repository_tool = getToolByName(portal, 'portal_repository')
+        repository_tool.setVersionableContentTypes('ftw.file.File')
+
+        self.portal.REQUEST.set('file', self.new_file)
+        self.file_.restrictedTraverse('ajax-upload')()
+
+        history = repository_tool.getHistory(self.file_)
+        self.assertEqual(1, len(history))
+
