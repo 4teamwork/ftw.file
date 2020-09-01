f73fc42c6ed61397e2b22ca96700544aecd0d23a
Nachtalb <info@nachtalb.io>
2020-09-01 09:35:32 +0200

    Improve testing of ajax upload and versioning

--- ftw/file/tests/test_ajax_upload.py
+++ ftw/file/tests/test_ajax_upload.py
@@ -6,6 +6,7 @@ from ftw.file.testing import FTW_FILE_INTEGRATION_TESTING
 from plone import api
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import setRoles
+from plone.protect.authenticator import createToken
 from unittest import TestCase
 from zExceptions import BadRequest
 import json
@@ -31,11 +32,15 @@ class TestAjaxUpload(TestCase):
 
     def test_replace_file(self):
         self.portal.REQUEST.set('file', self.new_file)
+        self.portal.REQUEST.set('_authenticator', createToken())
 
         ajax_upload_view = self.file_.restrictedTraverse('ajax-upload')
         response = json.loads(ajax_upload_view())
-
         self.assertEqual(response['success'], True)
+
+        self.new_file.seek(0)
+        self.assertEqual(self.file_.file.filename, self.new_file.filename)
+        self.assertEqual(self.file_.file.data, self.new_file.read())
 
     def test_no_file(self):
         with self.assertRaises(BadRequest):
@@ -46,9 +51,14 @@ class TestAjaxUpload(TestCase):
         repository_tool = getToolByName(portal, 'portal_repository')
         repository_tool.setVersionableContentTypes('ftw.file.File')
 
+        history = repository_tool.getHistory(self.file_)
+        self.assertEqual(0, len(history))
+
         self.portal.REQUEST.set('file', self.new_file)
+        self.portal.REQUEST.set('_authenticator', createToken())
         self.file_.restrictedTraverse('ajax-upload')()
 
         history = repository_tool.getHistory(self.file_)
         self.assertEqual(1, len(history))
-
+        self.assertEqual(u'File "test.pdf" replaced with "new_file.txt" via drag & drop.',
+                         history[0].comment)
