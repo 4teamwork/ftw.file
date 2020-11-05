ec1a5c578a6ef16acab92801b340d0ee5558a827
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2020-11-05 08:46:48 +0100

    Raise Invalid from zope.interface in order to have a meaningful error meassge in z3c.form.

--- ftw/file/tests/test_invalid_mimetypes.py
+++ ftw/file/tests/test_invalid_mimetypes.py
@@ -1,7 +1,12 @@
 from ftw.builder import create, Builder
 from ftw.file.tests import FunctionalTestCase
+from ftw.testbrowser import browsing
+from ftw.testbrowser.pages import factoriesmenu
 from plone.registry.interfaces import IRegistry
 from zope.component import getUtility
+from zope.interface import Invalid
+import os
+import transaction
 
 
 class TestInvalidMimeTypes(FunctionalTestCase):
@@ -20,7 +25,37 @@ class TestInvalidMimeTypes(FunctionalTestCase):
         self.assertIn(obj.getId(), self.portal)
 
         self.registry['ftw.file.filesettings.invalid_mimeteypes'] = [u'application/pdf', ]
-        with self.assertRaises(ValueError):
+        with self.assertRaises(Invalid):
             create(Builder('file')
                    .titled(u'Some title')
                    .attach_asset(u'test.pdf'))
+
+    @browsing
+    def test_show_meaningful_message_on_form(self, browser):
+        file_path = '{}/assets/test.pdf'.format(os.path.split(__file__)[0])
+
+        browser.login().visit()
+        factoriesmenu.add('File')
+        with open(file_path, 'r') as file_:
+            browser.fill({'Title': 'My file',
+                          'Filename': 'myfile',
+                          'File': (file_.read(),
+                                   'test.pdf')
+                          }).save()
+
+        self.assertIn(browser.context.getId(), self.portal)
+
+        self.registry['ftw.file.filesettings.invalid_mimeteypes'] = [u'application/pdf', ]
+        transaction.commit()
+        browser.login().visit()
+        with open(file_path, 'r') as file_:
+            factoriesmenu.add('File')
+            browser.fill({'Title': 'My file',
+                          'Filename': 'myfile',
+                          'File': (file_.read(),
+                                   'test.pdf')
+                          }).save()
+        self.assertEquals(
+            u'Invalid mime type: application/pdf is not allowed',
+            browser.css('.fieldErrorBox .error').first.text
+        )
