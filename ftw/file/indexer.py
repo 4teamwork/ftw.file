989b8f63a0aa0435096635fb928b458d9ab70409
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2021-10-21 16:24:33 -0400

    Add ftw.file.File SearchableText index.

--- ftw/file/indexer.py
+++ ftw/file/indexer.py
@@ -1,3 +1,5 @@
+from ftw.file.interfaces import IFile
+from plone.app.contenttypes.indexers import SearchableText_file
 from plone.indexer.decorator import indexer
 from Products.CMFPlone.utils import safe_hasattr
 from zope.interface import Interface
@@ -7,3 +9,8 @@ from zope.interface import Interface
 def document_date(obj):
     if safe_hasattr(obj, 'document_date'):
         return obj.document_date
+
+
+@indexer(IFile)
+def SearchableText_ftwfile(obj):
+    return SearchableText_file(obj)()
